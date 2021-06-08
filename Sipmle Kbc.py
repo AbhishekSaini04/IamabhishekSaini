9cb0d9343b2f75868dc0ea915478656b1dc8af35
IamabhishekSaini <85232708+IamabhishekSaini@users.noreply.github.com>
2021-06-08 11:16:37 +0530

    Create Sipmle Kbc.py

---
+++ Sipmle Kbc.py
@@ -0,0 +1,27 @@
+- 👋 Hi, I’m @IamabhishekSaini
+- 👀 I’m interested in ...
+- 🌱 I’m currently learning ...
+- 💞️ I’m looking to collaborate on ...
+- 📫 How to reach me ...
+
+<!---
+IamabhishekSaini/IamabhishekSaini is a ✨ special ✨ repository because its `README.md` (this file) appears on your GitHub profile.
+You can click the Preview link to take a look at your changes.
+--->
+print ("welcome to kbc game\n")
+print ("press 1 for playing\n")
+user_input= int(input())
+if user_input==1:
+	print("if you are bad then i am your dad-ye dilog kis film ka hai?")
+	print("1.Kgf\n2.jllb\n")
+	userinput=int(input("prees 1 for kgf or prees 2 for jllb\n"))
+	if userinput==1:
+		print("yes,kgf right answer")
+	elif userinput<=1:
+		print("wrong answer")
+	elif userinput>=1:
+		print("wrong answer")
+elif user_input<=1:
+	print("invelid input")
+elif user_input>=1:
+	print("invelid input")
